cba4297a6f4359f24501df987996faee1ab5dc6e
ToyB-Chan <jasjason0otto@gmail.com>
2023-02-10 09:40:08 +0100

    Changed L shape color

--- Tetromino_L_Shape.java
+++ Tetromino_L_Shape.java
@@ -1,9 +1,9 @@
 public class Tetromino_L_Shape extends Tetromino {
 	public Tetromino_L_Shape() {
 		this.blocks = new TetrominoBlock[4];
-		this.blocks[0] = new TetrominoBlock(0, 0, this, TerminalColor.DarkYellow);
-		this.blocks[1] = new TetrominoBlock(0, -1, this, TerminalColor.DarkYellow);
-		this.blocks[2] = new TetrominoBlock(0, -2, this, TerminalColor.DarkYellow);
-		this.blocks[3] = new TetrominoBlock(1, -2, this, TerminalColor.DarkYellow);
+		this.blocks[0] = new TetrominoBlock(0, 0, this, TerminalColor.DarkRed);
+		this.blocks[1] = new TetrominoBlock(0, -1, this, TerminalColor.DarkRed);
+		this.blocks[2] = new TetrominoBlock(0, -2, this, TerminalColor.DarkRed);
+		this.blocks[3] = new TetrominoBlock(1, -2, this, TerminalColor.DarkRed);
 	}
 }
