bdb1f87387a6c3586f7233bfd356a8757a34093c
ToyB-Chan <jasjason0otto@gmail.com>
2023-02-14 16:42:05 +0100

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-START powershell.exe -Command "javac ./Main.java; Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted -Force; ./set-console-mode.ps1; java -cp . Main | Out-Host"
+START powershell.exe -NoExit -Command "javac ./Main.java; Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted -Force; ./set-console-mode.ps1; java -cp . Main | Out-Host"
