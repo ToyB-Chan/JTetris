13d0b483b6b56e4be9734da5ea81d4f9d1e19161
Seraph-V <vale.jurke@gmail.com>
2023-02-14 11:54:20 +0100

    started implementing setRotation

--- Tetromino_I_Shape.java
+++ Tetromino_I_Shape.java
@@ -1,8 +1,27 @@
 public class Tetromino_I_Shape extends Tetromino {
-	public Tetromino_I_Shape() {
-		this.blocks[0] = new TetrominoBlock(0, 0, this, TerminalColor.Cyan);
-		this.blocks[1] = new TetrominoBlock(0, -1, this, TerminalColor.Cyan);
-		this.blocks[1] = new TetrominoBlock(0, -2, this, TerminalColor.Cyan);
-		this.blocks[1] = new TetrominoBlock(0, -3, this, TerminalColor.Cyan)
+	public Tetromino_I_Shape(int rotation) {
+		this.blocks  = new TetrominoBlock[4];
+		for (int i = 0; i < 3; i++) { 
+			this.blocks[i] = new TetrominoBlock(0, 0, this, TerminalColor.Cyan); 
+		}
+
+		this.setRotation(rotation);	
+	}
+
+	@Override
+	public void setRotation(int rotation) {
+		this.rotation = rotation;
+
+		switch (rotation) {
+			case 0:
+				this.blocks[0].setRelativeLocation(0, 0);
+				break;
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+		}
 	}
 }
