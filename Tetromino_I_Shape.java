87e572e3665d42faa4245b0c6c017498c5064f59
Seraph-V <vale.jurke@gmail.com>
2023-02-14 12:01:45 +0100

    I Shape ra

--- Tetromino_I_Shape.java
+++ Tetromino_I_Shape.java
@@ -14,13 +14,18 @@ public class Tetromino_I_Shape extends Tetromino {
 
 		switch (rotation) {
 			case 0:
+			case 2:
 				this.blocks[0].setRelativeLocation(0, 0);
+				this.blocks[1].setRelativeLocation(0, -1);
+				this.blocks[2].setRelativeLocation(0, -2);
+				this.blocks[3].setRelativeLocation(0, -3);
 				break;
 			case 1:
-				break;
-			case 2:
-				break;
-			case 3:
+			case 3: 
+				this.blocks[0].setRelativeLocation(0, 0);
+				this.blocks[1].setRelativeLocation(1, 0);
+				this.blocks[2].setRelativeLocation(2, 0);
+				this.blocks[3].setRelativeLocation(3, 0);
 				break;
 		}
 	}
