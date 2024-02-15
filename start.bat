aa7580995a7a801fe64816c64db85ae35fe085f6
ToyB-Chan <jasjason0otto@gmail.com>
2024-02-15 19:43:40 +0100

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,2 +1,3 @@
 @ECHO OFF
+MKDIR out
 START powershell.exe -NoExit -Command "javac -d ./out/ ./src/*.java; Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted -Force; ./set-console-mode.ps1; java -cp ./out/ Main | Out-Host"
